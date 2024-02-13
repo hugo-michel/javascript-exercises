e8332511904b8223d1f12a457ed4c8fa913e60b7
hugo-michel <orayito62@gmail.com>
2024-02-13 02:47:03 +0100

    exo 4 odin js loop et array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(numbers, ...arg) {
+    // let result = numbers.filter((number) => !(number === arg));
+    let result = numbers.filter((number) => !arg.includes(number));
+    return result;
 };
 
 // Do not edit below this line
