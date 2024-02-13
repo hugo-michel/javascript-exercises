247c3515fe6b5f5499c2f5410a1673922cece1da
hugo-michel <orayito62@gmail.com>
2024-02-13 03:07:51 +0100

    exo 5 js odin loop et array

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let result = 0;
 
+    if ((typeof num1 != "number") || (typeof num2 != "number") || num1 < 0 || num2 < 0) {
+        return "ERROR"
+    }
+
+    if (num1 < num2) {
+        for (num1; num1 <= num2; num1++) {
+            result += num1;
+        }
+    } else if (num1 > num2) {
+        for (num2; num2 <= num1; num2++) {
+            result += num2;
+        }
+    }
+
+
+    return result;
 };
 
 // Do not edit below this line
