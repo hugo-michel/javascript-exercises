7e1a95fd7ef8c56d49002d090896ed4af2b019f3
hugo-michel <orayito62@gmail.com>
2024-02-13 03:41:26 +0100

    exo 7 js odin

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let nombre = (temp - 32) * (5/9);
+  let nombreArrondi = Math.round(nombre * 10) / 10
+  return nombreArrondi;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let nombre = (temp * (9/5)) + 32;
+  let nombreArrondi = Math.round(nombre * 10) / 10
+  return nombreArrondi;
 };
 
 // Do not edit below this line
