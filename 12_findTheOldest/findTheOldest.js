ed190e091ed1d9e2a44e10189049f59c5cb09a59
hugo-michel <orayito62@gmail.com>
2024-02-17 05:44:59 +0100

    findTheOldest odin js

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+	let orderedByAge = array.sort((a, b) => {
+        if (!a.yearOfDeath) {
+            a.yearOfDeath = new Date().getFullYear();
+        }
+        if (!b.yearOfDeath) {
+            b.yearOfDeath = new Date().getFullYear();
+        }
+		if ((a.yearOfDeath - a.yearOfBirth) > (b.yearOfDeath - b.yearOfBirth)) {
+			return -1;
+		} else {
+			return 1;
+		}
+	});
+    return orderedByAge[0];
 };
 
 // Do not edit below this line
