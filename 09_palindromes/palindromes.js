2555af7dd58cab5d6ac11ca867ea6f2a2991471a
hugo-michel <orayito62@gmail.com>
2024-02-17 04:34:02 +0100

    exo palindrome js odin

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let regex = /[a-zA-Z0-9]+/g;
+    let lettreUniquement = str.match(regex).join("").toLowerCase();
+    let lettreUniquementInverse = lettreUniquement.split("").reverse().join("");
+    return lettreUniquement === lettreUniquementInverse ? true : false;
 };
 
 // Do not edit below this line
