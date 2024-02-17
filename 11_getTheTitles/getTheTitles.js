eed9f23139631623d49468f7bd5af71ffb243377
hugo-michel <orayito62@gmail.com>
2024-02-17 05:01:20 +0100

    odin exo book js

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    let booksTitles = array.map(book => book["title"])
+    return booksTitles;
 };
 
 // Do not edit below this line
